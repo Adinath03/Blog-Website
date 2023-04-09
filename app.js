c6658a78889ed1947714022d926ac5d460ad711a
Adinath03 <adinathmahandule.07@gmail.com>
2023-04-09 21:11:03 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,60 +1,99 @@
-//jshint esversion:6
-
+const port = process.env.PORT || 3000;
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 const { forEach } = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts = [];
 const app = express();
-app.set('view engine', 'ejs');
+require("dotenv").config();
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const homeStartingContent =
+  "Welcome to my blog! Here you will find a collection of my thoughts and experiences in the world of technology. I love building and creating applications, and I'm always looking for ways to solve problems and improve user experiences. Whether you're interested in android development, web development, or machine learning, there's something for everyone here. So come on in and explore!";
+const aboutContent =
+  "Hi! I'm Adinath Mahandule, a final year BE student from Ramrao Adik Institute of Technology majoring in computer science. I've always been fascinated by technology since I was a child. Building and creating applications tap into my creative edge, and I enjoy solving the problems I encounter along the way. I have developed numerous projects during my engineering curriculum in domains like android development, web development, and machine learning. My preferred programming language is C++. I have recently also done some project work in Python. I like to think positively & work well with teams, and I'm always eager to learn new things. In my free time, I enjoy going for a walk or a hike in nature. I also love to read mystery and mythological books.";
+const contactContent =
+  "If you have any questions or suggestions, please feel free to reach out to me using the contact form below. I'm always happy to hear from my readers and to help out in any way that I can. You can also follow me on social media to stay up to date with my latest projects and adventures. Thank you for visiting my blog, and I look forward to hearing from you!";
 
-app.get("/", function (req, res) {
-  res.render("home", { startingContent: homeStartingContent, posts: posts })
+//Connecting mongodb with app
+// mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
+
+//connecting with mongoose atlas
+mongoose.connect(`mongodb+srv://${process.env.ADMIN_USERNAME}:${process.env.ADMIN_PASSWORD}@cluster0.sdwodvq.mongodb.net/todolistDB?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
-app.get('/posts/:title', (req, res) => {
-  const postTitle = _.lowerCase(req.params.title);
-  posts.forEach(function (post) {
-    const currentTitle = _.lowerCase(post.title);
-    if (currentTitle == postTitle) {
-      // console.log("Match Found");
-      res.render("post", { title: post.title, content: post.content });
-    } else {
-      // console.log("Match not found");
-    }
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
   })
-})
+  .catch((err) => {
+    console.log('Error connecting to MongoDB Atlas: ', err);
+  });
+
+//schema for our posts
+const postSchema = {
+  title: String,
+  content: String
+};
+
+//creating database using postSchema
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", async function (req, res) {
+  try {
+    const posts = await Post.find({});
+    //console.log(posts);
+    res.render("home", { startingContent: homeStartingContent, posts: posts });
+  } catch (error) {
+    console.log(err);
+    res.redirect("/");
+  }
+});
+
+app.get("/posts/:title", async (req, res) => {
+  try {
+    const postTitle = _.lowerCase(req.params.title);
+    const posts = await Post.find({});
+    posts.forEach(function (post) {
+      const currentTitle = _.lowerCase(post.title);
+      if (currentTitle == postTitle) {
+        res.render("post", { title: post.title, content: post.content });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 app.get("/about", function (req, res) {
-  res.render("about", { aboutContent: aboutContent })
-})
+  res.render("about", { aboutContent: aboutContent });
+});
 
 app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent })
-})
+  res.render("contact", { contactContent: contactContent });
+});
 
 app.get("/compose", function (req, res) {
   res.render("compose");
-})
+});
 
 app.post("/compose", function (req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postContent
-  }
-  posts.push(post);
+  const title = req.body.postTitle;
+  const content = req.body.postContent;
+
+  const newPost = new Post({
+    title: title,
+    content: content
+  });
+
+  newPost.save();
   res.redirect("/");
-})
+});
 
-
-
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`);
 });
